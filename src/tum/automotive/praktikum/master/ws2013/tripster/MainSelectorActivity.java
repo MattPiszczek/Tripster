1bcb127e281b99ee741fda72c4f03e1e2e9662fb
MattPiszczek <piszczek.mat@gmail.com>
2014-03-07 17:14:32 +0100

    This branch is main branch for our developement code. Buttons from main screen should simply starts you main activities. Please create separate branch for features that you are currently developing.

---
+++ src/tum/automotive/praktikum/master/ws2013/tripster/MainSelectorActivity.java
@@ -0,0 +1,70 @@
+package tum.automotive.praktikum.master.ws2013.tripster;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import android.os.Build;
+
+public class MainSelectorActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_selector);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_selector, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main_selector,
+					container, false);
+			return rootView;
+		}
+	}
+	
+	public void onClickFarDistanceNavi(View v) {
+		Toast.makeText(this, "FarDistanceNavi", Toast.LENGTH_SHORT).show();
+
+	}
+
+}
