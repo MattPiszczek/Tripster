c631b22ade1ccbd500f1498d2e9704352a4db8f8
MattPiszczek <piszczek.mat@gmail.com>
2014-03-09 16:07:41 +0100

    Working and ready for meargin with samples.

---
+++ src/tum/automotive/praktikum/master/ws2013/tripster/navi/far/CloseNaviNoARFragment.java
@@ -0,0 +1,7 @@
+package tum.automotive.praktikum.master.ws2013.tripster.navi.far;
+
+import android.app.Fragment;
+
+public class CloseNaviNoARFragment extends Fragment{
+
+}
--- src/tum/automotive/praktikum/master/ws2013/tripster/navi/menu/NavigateWithMapsAPIDevActivity.java
+++ src/tum/automotive/praktikum/master/ws2013/tripster/navi/menu/NavigateWithMapsAPIDevActivity.java
@@ -1,24 +1,24 @@
-package tum.automotive.praktikum.master.ws2013.tripster;
+package tum.automotive.praktikum.master.ws2013.tripster.navi.menu;
 
-import tum.automotive.praktikum.master.ws2013.tripster.navi.menu.NavigateWithMapsAPIDevActivity;
-import android.app.Activity;
-import android.app.ActionBar;
+
+import tum.automotive.praktikum.master.ws2013.tripster.R;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Toast;
 
-public class MainSelectorActivity extends Activity {
+public class NavigateWithMapsAPIDevActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main_selector);
+		setContentView(R.layout.activity_navigate_with_maps_apidev);
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
@@ -30,7 +30,7 @@ public class MainSelectorActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_selector, menu);
+		getMenuInflater().inflate(R.menu.navigate_with_maps_apidev, menu);
 		return true;
 	}
 
@@ -57,15 +57,23 @@ public class MainSelectorActivity extends Activity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main_selector,
-					container, false);
+			View rootView = inflater.inflate(
+					R.layout.fragment_navigate_with_maps_apidev, container,
+					false);
 			return rootView;
 		}
 	}
 	
-	public void onClickFarDistanceNavi(View v) {
-		Intent intentFarNaviDev = new Intent(this, NavigateWithMapsAPIDevActivity.class);
-		startActivity(intentFarNaviDev);
+	public void onClickTargetFromMap(View v) {
+		Toast.makeText(this, "From Map", Toast.LENGTH_SHORT).show();
+	}
+	
+	public void onClickHpbfAsTarget(View v) {
+		Toast.makeText(this, "Go to Hpbf Munich", Toast.LENGTH_SHORT).show();
+	}
+	
+	public void onClickLocationAsTarget(View v) {
+		Toast.makeText(this, "Location as target", Toast.LENGTH_SHORT).show();
 	}
 
 }
