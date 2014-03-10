95d12fd40accbf75daa6a91792565b6d4a9987c4
MattPiszczek <piszczek.mat@gmail.com>
2014-03-10 19:36:27 +0100

    Calculating haversine, the distance between two pairs latitude/longitude.

--- src/tum/automotive/praktikum/master/ws2013/tripster/navi/util/LocationUtils.java
+++ src/tum/automotive/praktikum/master/ws2013/tripster/navi/util/LocationUtils.java
@@ -89,4 +89,28 @@ public final class LocationUtils {
             return EMPTY_STRING;
         }
     }
+    
+    private final static double AVERAGE_RADIUS_OF_EARTH = 6371009;
+    private int calculateDistance(double userLat, double userLng, double venueLat, double venueLng) {
+
+        double latDistance = Math.toRadians(userLat - venueLat);
+        double lngDistance = Math.toRadians(userLng - venueLng);
+
+        double a = (Math.sin(latDistance / 2) * Math.sin(latDistance / 2)) +
+                        (Math.cos(Math.toRadians(userLat))) *
+                        (Math.cos(Math.toRadians(venueLat))) *
+                        (Math.sin(lngDistance / 2)) *
+                        (Math.sin(lngDistance / 2));
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return (int) (Math.round(AVERAGE_RADIUS_OF_EARTH * c));
+
+    }
+    
+    public int calculateDistance(double userLat, double userLng, String venueLat, String venueLng) {
+    	double targetLat = new Double(venueLat);
+    	double targetLng = new Double(venueLng);
+    	return calculateDistance(userLat, userLng, targetLat, targetLng);
+    }
 }
